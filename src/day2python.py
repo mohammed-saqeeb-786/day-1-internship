758c0344b32c81045f24fb4b810594cc5474864b
mohammed saqeeb <mohammedsaqeeb1312@gmail.com>
2026-02-04 22:38:06 +0530

    day-1 internship

---
+++ src/day2python.py
@@ -0,0 +1,22 @@
+#task 1
+user_name=input("enter your name:")
+user_age=(input("enter your age"))
+user_age=int(user_age)
+user_age=user_age+4
+print(f"hey{user_name},you will be{user_age}year old in 2030")
+
+#task 2
+totalbill=float(input("enter your amount"))
+number_of_people=int(input("enter your number of people"))
+share_per_person=totalbill/number_of_people
+print(f"{totalbill}each  person pay{share_per_person}")
+  
+#task 3
+item_name="laptop"
+quantity=2
+price=499.9
+in_stock=True
+print(item_name,quantity,price,in_stock)
+totalcost=quantity*price
+print(totalcost)
+
